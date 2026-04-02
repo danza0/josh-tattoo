ebceab4782d431e4e5b36e3250088188ef1dd0c1
Danyil Dushuk <dushukdanyil@gmail.com>
2026-04-02 13:30:06 -0700

    start bust rotation immediately on first scroll

--- components/StatueSection.tsx
+++ components/StatueSection.tsx
@@ -12,17 +12,18 @@ export default function StatueSection() {
     offset: ["start start", "end end"],
   });
 
-  const HERO_OVERLAP_RATIO = 1 / 3;
-  const frameProgress = useTransform(scrollYProgress, [HERO_OVERLAP_RATIO, 1], [0, 1]);
+  // Frame rotation starts immediately from the very first scroll
+  const frameProgress = useTransform(scrollYProgress, [0, 1], [0, 1]);
 
-  const aboutOpacity = useTransform(scrollYProgress, [0.33, 0.43, 0.57, 0.67], [0, 1, 1, 0]);
-  const aboutY = useTransform(scrollYProgress, [0.33, 0.43], ["40px", "0px"]);
+  // Text overlays appear in the second half of the scroll
+  const aboutOpacity = useTransform(scrollYProgress, [0.4, 0.5, 0.6, 0.7], [0, 1, 1, 0]);
+  const aboutY = useTransform(scrollYProgress, [0.4, 0.5], ["40px", "0px"]);
 
-  const sidenoteOpacity = useTransform(scrollYProgress, [0.53, 0.63, 0.77, 0.83], [0, 1, 1, 0]);
-  const sidenoteY = useTransform(scrollYProgress, [0.53, 0.63], ["30px", "0px"]);
+  const sidenoteOpacity = useTransform(scrollYProgress, [0.55, 0.65, 0.75, 0.82], [0, 1, 1, 0]);
+  const sidenoteY = useTransform(scrollYProgress, [0.55, 0.65], ["30px", "0px"]);
 
-  const philosophyOpacity = useTransform(scrollYProgress, [0.73, 0.83, 0.93, 1], [0, 1, 1, 0]);
-  const philosophyY = useTransform(scrollYProgress, [0.73, 0.83], ["40px", "0px"]);
+  const philosophyOpacity = useTransform(scrollYProgress, [0.75, 0.85, 0.93, 1], [0, 1, 1, 0]);
+  const philosophyY = useTransform(scrollYProgress, [0.75, 0.85], ["40px", "0px"]);
 
   return (
     <section
